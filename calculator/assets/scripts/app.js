c81c1beea4c4437a1790068f8725ea1af25f2cb8
IvanovE <EugeneIvanov12@gmail.com>
2021-11-13 12:51:54 +0300

    app and index changed

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0
 let currentResult = defaultResult
+let logEntries = []
 
 
 // Gets input from input field
@@ -8,37 +9,53 @@ function getUserNumberInput() {
 }
 
 // Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNubmer) {
+function renderResult(operator, resultBeforeCalc, calcNubmer) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNubmer}`
   outputResult(currentResult, calcDescription) // from vendor file
+}
+
+//
+function renderLog(operation, prevResult, calcNumber, newResult) {
+  const logEntry = {
+    operation: operation,
+    prevResult: prevResult,
+    calcNumber: calcNumber,
+    newResult: newResult
+  }
+  logEntries.push(logEntry)
+  console.log(logEntries)
 }
 
 function add() {
   const enteredNumber = getUserNumberInput()
   const initialResult = currentResult
-  currentResult = currentResult + enteredNumber
-  createAndWriteOutput('+', initialResult, enteredNumber)
+  currentResult += enteredNumber
+  renderResult('+', initialResult, enteredNumber)
+  renderLog('ADD', initialResult, enteredNumber, currentResult)
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput()
   const initialResult = currentResult
-  currentResult = currentResult - enteredNumber
-  createAndWriteOutput('-', initialResult, enteredNumber)
+  currentResult -= enteredNumber
+  renderResult('-', initialResult, enteredNumber)
+  renderLog('SUBTRACT', initialResult, enteredNumber, currentResult)
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput()
   const initialResult = currentResult
-  currentResult = currentResult / enteredNumber
-  createAndWriteOutput('/', initialResult, enteredNumber)
+  currentResult /= enteredNumber
+  renderResult('/', initialResult, enteredNumber)
+  renderLog('DIVIDE', initialResult, enteredNumber, currentResult)
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput()
   const initialResult = currentResult
-  currentResult = currentResult * enteredNumber
-  createAndWriteOutput('*', initialResult, enteredNumber)
+  currentResult *= enteredNumber
+  renderResult('*', initialResult, enteredNumber)
+  renderLog('MULTIPLY', initialResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click', add)
